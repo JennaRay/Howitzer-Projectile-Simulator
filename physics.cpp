2252e840960c3ba575532f137e92539497ca3674
Jenna Ray <113537049+JennaRay@users.noreply.github.com>
2025-05-07 12:47:17 -0600

    Update physics.cpp

--- physics.cpp
+++ physics.cpp
@@ -2,7 +2,7 @@
  * Source File:
  *    PHYSICS
  * Author:
- *    <your name here>
+ *    Jenna Ray
  * Summary:
  *    Laws of motion, effects of gravity, wind resistence, etc.
  ************************************************************************/
